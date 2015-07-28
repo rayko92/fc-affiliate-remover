2b33745fb1b4c96b48780c21d369323450e50fc6
Topo <Thetopox@gmail.com>
2015-07-28 04:47:02 +0200

    Performance improvement
    Improved performance and code

--- fc_affiliate_remover.user.js
+++ fc_affiliate_remover.user.js
@@ -5,20 +5,6 @@
 // @match        *://*.forocoches.com/foro/*
 // @include      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @grant        none
+// @run-at document-end
 // ==/UserScript==
-$(document).ready(function(){
- $("div[name=\"HOTWordsTxt\"]").each(function(){
- 	var html = $(this);
-
- 	$('a', html).each(function(){
- 		var href 	= $(this).attr('href');
- 		var href2 	= href.split('link.php?url=');
- 		var decode  = decodeURIComponent(href2[1]);
-
- 		if(typeof href2[1] !== 'undefined'){
- 			$(this).attr('href', decode);
- 			console.log(decode);
- 		}
- 	});
- });
-});
+$('a[href*="url="]').each(function(){$(this).attr("href",decodeURIComponent($(this).attr("href").split("url=")[1]))});
