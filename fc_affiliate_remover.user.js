e14777a5d5e91f37b30ef49d23ed3afde27a8804
Topo <Thetopox@gmail.com>
2015-07-28 05:27:14 +0200

    Improvements
    Changed split with substr (faster)

--- fc_affiliate_remover.user.js
+++ fc_affiliate_remover.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         FC Aff Remove
-// @version      0.1
+// @version      0.2
 // @description  Reemplazo de links de afiliados
 // @match        *://*.forocoches.com/foro/*
 // @grant        none
 // @run-at document-end
 // ==/UserScript==
-var l=document.querySelectorAll('a[href*="url="]');for(i=0;i<l.length;i++)l[i].href=decodeURIComponent(l[i].href.split("url=")[1]);
+var l=document.querySelectorAll('a[href*="url="]');for(i=0;i<l.length;i++)l[i].href=decodeURIComponent(l[i].href.substr(l[i].href.indexOf("url=")));
