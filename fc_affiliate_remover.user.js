4ce6eaffeb1771363637f1669e9e464f3456bd17
Topo <Thetopox@gmail.com>
2015-07-28 17:58:25 +0200

    Fixed problem with url

--- fc_affiliate_remover.user.js
+++ fc_affiliate_remover.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         FC Aff Remove
-// @version      0.2
+// @version      0.3
 // @description  Reemplazo de links de afiliados
 // @match        *://*.forocoches.com/foro/*
 // @grant        none
 // @run-at document-end
 // ==/UserScript==
-var l=document.querySelectorAll('a[href*="url="]');for(i=0;i<l.length;i++)l[i].href=decodeURIComponent(l[i].href.substr(l[i].href.indexOf("url=")));
+var l=document.querySelectorAll('a[href*="url="]');for(i=0;i<l.length;i++)l[i].href=decodeURIComponent(l[i].href.substr(l[i].href.indexOf("url=")+4));
